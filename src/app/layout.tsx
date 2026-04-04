2cd2f7c663ea3a0c1de5e621e7d0d9d3bbee51ae
Paulo Henrique Vieira Candido <phvcandido@gmail.com>
2026-04-04 14:46:02 -0300

    Add ms clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -62,6 +63,19 @@ export default function RootLayout({
               ],
               "description": "Comunidade de desenvolvedores de Itapetininga e região."
             }),
+          }}
+        />
+        <Script
+          id="microsoft-clarity"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "w6ja6qwlko");
+            `,
           }}
         />
       </head>
